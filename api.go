592e2f8cd4f95a248c2651d9b4eb74098b3a43b2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-22 16:20:15 +0530

    adding api servers and http handlers

--- api.go
+++ api.go
@@ -1,6 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+  w.WriteHeader(status) 
+  w.Header().Set("Content Type", "application/json")
+  return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error 
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
+  return func(w http.ResponseWriter, r *http.Request) {
+    if err := f(w, r); err != nil {
+      //handle the error 
+    }
+  }
+}
 
 type APIServer struct {
   listenAddr string
@@ -10,6 +31,12 @@ func newAPIServer(listenAddr string) *APIServer {
   return &APIServer{
     listenAddr: listenAddr,
   }
+}
+
+func (s *APIServer) Run() {
+  router := mux.NewRouter()
+
+  router HandleFunc("/account", s.handleAccount)
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error  {
